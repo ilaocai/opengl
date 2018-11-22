a379bd552b6aa02d23ba573f79d2e388a6525b74
ilaocai <kohou.hu@aliyun.com>
2018-11-22 13:41:12 +0800

    feat: change license

--- example/hello/src/main.cpp
+++ example/hello/src/main.cpp
@@ -1,25 +1,46 @@
+/**
+ * Copyright 2018 Hu Peng
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include <GLFW/glfw3.h>
 #include <iostream>
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+{
   glViewport(0, 0, width, height);
 }
 
-void processInput(GLFWwindow* window) {
-  if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS) {
+void processInput(GLFWwindow *window)
+{
+  if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
+  {
     glfwSetWindowShouldClose(window, true);
   }
 }
 
-int main(int argc, char const* argv[]) {
+int main(int argc, char const *argv[])
+{
   glfwInit();
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
   //   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-  GLFWwindow* window = glfwCreateWindow(800, 600, "hello", NULL, NULL);
-  if (window == NULL) {
+  GLFWwindow *window = glfwCreateWindow(800, 600, "hello", NULL, NULL);
+  if (window == NULL)
+  {
     std::cout << "Failed to create GLFW window" << std::endl;
     glfwTerminate();
     return -1;
@@ -30,7 +51,8 @@ int main(int argc, char const* argv[]) {
   glViewport(0, 0, 800, 600);
   glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-  while (!glfwWindowShouldClose(window)) {
+  while (!glfwWindowShouldClose(window))
+  {
     processInput(window);
 
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
